1280b8e2a71119de4dfe077dd8660544c796d0eb
eMobilis <samuelnjihia81@gmail.com>
2023-06-26 09:25:52 +0300

    first commit

---
+++ calc_area.py
@@ -0,0 +1,12 @@
+#enable yser to enter input
+height =input("Enter height")
+width = input("Enter width")
+
+#covert two variables into intagers
+height =int(height)
+width =int(width)
+
+#perform calculation
+result =height * width
+print("Area is",  result)
+
