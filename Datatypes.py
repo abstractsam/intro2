ad0b8e25ee01abc7a482a85a189ba44701434371
eMobilis <samuelnjihia81@gmail.com>
2023-06-26 10:04:17 +0300

    Datatypes

---
+++ Datatypes.py
@@ -0,0 +1,4 @@
+city = "Kisumu" #string
+age =15  #Int
+temp = 36.1523 #float
+
---
+++ bmicalc.py
@@ -0,0 +1,17 @@
+#create two varibles weight and height
+weight =input("Enter w in kgs")
+height =input("Enter h in meters")
+
+
+weight = float(weight)
+height = float(height)
+
+#perform calculation
+result =weight / (height*height)
+print("Your BMI is", result)
+
+
+
+
+
+
